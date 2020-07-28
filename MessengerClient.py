fbd0367436335dd50736daa586484a0269262c48
Trung-Rei <trantrung.trantrung.tt@gmail.com>
2020-07-28 10:50:37 +0700

    Đã fix được bug, nhưng chưa hoàn thiện tính năng

--- MessengerClient.py
+++ MessengerClient.py
@@ -3,6 +3,8 @@ from tkinter import scrolledtext
 from tkinter import messagebox
 import socket
 import threading
+import sys
+import gc
 
 class SignUp:
     def __init__(self, s_send):
@@ -142,6 +144,7 @@ class Messenger:
     def run(self):
         self.gui.mainloop()
         x = 1
+        return
 
     def send_message(self):
         text = self.st_send.get('1.0', END)
@@ -165,7 +168,7 @@ class Messenger:
         self.st_recv.configure(state = 'disabled')
         self.st_recv.see('end')
 
-    def close_window(self):
+    def _close_window(self):
         self.s_send.send(b'PRIVCHAT')
         self.s_send.recv(1024)
         self.s_send.send(b'DEL\n')
@@ -173,7 +176,7 @@ class Messenger:
         self.s_send.send(self.roomName.encode())
         self.s_send.recv(1024)
     
-    def _close_window(self):
+    def close_window(self):
         self.gui.destroy()
 
 class MainRoom(Messenger):
@@ -201,17 +204,20 @@ class RoomList:
     rooms = {}
     s_send = None
 
-    def addRoom(self, roomName):
-        t = threading.Thread(target=self.createRoom_thread, args=(roomName,))
-        t.start()
+def addRoom(roomName):
+    t = threading.Thread(target=createRoom_thread, args=(roomName,))
+    t.start()
 
-    def createRoom_thread(self, roomName):
-        self.rooms[roomName[0]] = Messenger(self.s_send, roomName[0])
-        self.rooms[roomName[0]].run()
+def createRoom_thread(roomName):
+    RoomList.rooms[roomName[0]] = Messenger(RoomList.s_send, roomName[0])
+    RoomList.rooms[roomName[0]].run()
+    RoomList.rooms.pop(roomName[0])
+    x = 1
 
-    def delRoom(self, roomName):
-        self.rooms[roomName].gui.destroy()
-        self.rooms.pop(roomName)
+def delRoom(roomName):
+    RoomList.rooms[roomName]._close_window()
+    RoomList.rooms.pop(roomName)
+    gc.collect()
 
 def main():
     host = '127.0.0.1'
@@ -263,12 +269,14 @@ def priv_chat(s_recv):
     if roomName == 'QUIT':
         roomName = s_recv.recv(1024).decode()
         s_recv.send(b'OK')
-        rlist = RoomList()
-        rlist.delRoom(roomName)
+        """rlist = RoomList()
+        rlist.delRoom(roomName)"""
+        delRoom(roomName)
         return
     roomName = roomName.split(' ', 1)
-    rlist = RoomList()
-    rlist.addRoom(roomName)
+    """rlist = RoomList()
+    rlist.addRoom(roomName)"""
+    addRoom(roomName)
 
 if __name__ == "__main__":
     main()
